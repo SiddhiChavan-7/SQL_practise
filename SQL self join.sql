5072c9daa78c300c46f8f1559189f18dfebf440e
Siddhi Nitin Chavan <chavansiddhi487@gmail.com>
2025-12-18 23:25:56 +0530

    Update SQL self join.sql

--- SQL self join.sql
+++ SQL self join.sql
@@ -4,3 +4,12 @@
 update parent set referred_by_id = 1 where id in(2,3,4);
 update parent set referred_by_id = 2 where id = 6;
 select * from parent;
+
+
+SELECT
+    a.id,
+    a.name AS user_name,
+    b.name AS referred_by_name
+FROM parent a
+LEFT JOIN parent b
+    ON a.referred_by_id = b.id;
